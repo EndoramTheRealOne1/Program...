f6c96d24ec5f2d7155befca2abc27aee4e2abec7
EndoramTheRealOne1 <86573194+EndoramTheRealOne1@users.noreply.github.com>
2021-06-27 23:17:27 +0300

    Add files via upload

---
+++ Open Me!.bat
@@ -0,0 +1,61 @@
+@echo off
+title Program...?
+color 1f
+echo Worm
+echo Worm2
+echo Worm3
+echo Worm4
+pause
+cls
+pause
+echo Why am i talking like this? Idk.
+echo Well, i will open up a game link... I hope that becomes your favorite game!
+pause
+start chrome.exe https://fishlicka.itch.io/juice-galaxy
+pause
+echo I will create a Text file now named Dark!
+pause
+echo Well well well... Seems like you came for me. This Text file has no purpose, except to fill up your hard drive... >> Dark.txt
+color 0c
+echo Also, imagine dragons.
+echo Loading animation
+ping localhost -n 2 > nul
+cls
+echo Loading animation.
+ping localhost -n 2 > nul
+cls
+echo Loading animation..
+ping localhost -n 2 > nul
+cls
+echo Loading animation...
+ping localhost -n 2 > nul
+cls
+echo Loading animation
+ping localhost -n 2 > nul
+cls
+echo Loading animation.
+ping localhost -n 2 > nul
+cls
+echo Loading animation..
+ping localhost -n 2 > nul
+cls
+echo Loading animation...
+ping localhost -n 2 > nul
+cls
+echo Loading animation
+ping localhost -n 2 > nul
+cls
+echo Loading animation.
+ping localhost -n 2 > nul
+cls
+echo Loading animation..
+ping localhost -n 2 > nul
+cls
+echo Loading animation...
+ping localhost -n 2 > nul
+cls
+
+:P
+echo IMAGINE DRAGONS THESE NUTS ON YO FACE BI-
+goto :P
+pause

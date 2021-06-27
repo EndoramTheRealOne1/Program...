d242e62e6e5d5c447aecec382b3623dc0d29e158
Mr. Mint <86573194+EndoramTheRealOne1@users.noreply.github.com>
2021-06-28 00:57:10 +0300

    Add files via upload
    The second version of ''Program...''!

--- Open Me!.bat
+++ Open Me!.bat
@@ -16,7 +16,17 @@ pause
 echo I will create a Text file now named Dark!
 pause
 echo Well well well... Seems like you came for me. This Text file has no purpose, except to fill up your hard drive... >> Dark.txt
+color 5f
+echo Let me show you a funny video!
+pause
+start chrome.exe https://www.youtube.com/watch?v=YG8Q7lzZ-PI
+echo Do you know who joe is?
+ping localhost -n 10 > nul
+cls
+echo I made you waste time. :)
+pause
 color 0c
+pause
 echo Also, imagine dragons.
 echo Loading animation
 ping localhost -n 2 > nul

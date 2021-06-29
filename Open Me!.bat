4d627f85a8033b122377158fa4328fa263c95c6e
Mr. Mint <86573194+EndoramTheRealOne1@users.noreply.github.com>
2021-06-29 15:47:19 +0300

    Add files via upload
    Added a second path!

--- Open Me!.bat
+++ Open Me!.bat
@@ -1,5 +1,17 @@
 @echo off
 title Program...?
+echo The final version is here... You will have two options to go a path...
+pause
+echo Say 1 to go the old path...
+echo Or... say 2 to go to the new path...
+
+
+set/p path=
+
+
+if %path% == 1 goto path1
+if %path% == 2 goto path2
+:path1
 color 1f
 echo Worm
 echo Worm2
@@ -68,4 +80,263 @@ cls
 :P
 echo IMAGINE DRAGONS THESE NUTS ON YO FACE BI-
 goto :P
+
+
+
+
+
+
+:path2
+echo Hi!
+pause
+echo Who are you?
+pause
+echo Well... Thats weird...
+pause
+echo Let me take you to a link! It will be fun!
+pause
+start chrome.exe https://www.youtube.com/watch?v=dQw4w9WgXcQ
+echo HA! I got you!
+pause
+echo :)
+echo This path was a big trol-
+pause
+color 0c
+echo Well well well... Im glad that annoying brat was gone...
+pause
+echo I couldent live with him at all...
+pause
+echo Who brings you here?
+pause
+echo Ehh, i couldent care less.
+cls
+echo Well... Now what...?
+pause
+echo I know what i will do... :)
+pause
+
+echo We're no strangers to love
+ping localhost -n 5 > nul
+cls
+
+echo You know the rules and so do I
+ping localhost -n 5 > nul
+cls
+
+echo A full commitment's what I'm thinking of
+ping localhost -n 5 > nul
+cls
+
+echo You wouldn't get this from any other guy
+ping localhost -n 5 > nul
+cls
+
+echo I just wanna tell you how I'm feeling
+ping localhost -n 5 > nul
+cls
+
+echo Gotta make you understand
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give you up
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna let you down
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna run around and desert you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna make you cry
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna say goodbye
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna tell a lie and hurt you
+ping localhost -n 5 > nul
+cls
+
+echo We've known each other for so long
+ping localhost -n 5 > nul
+cls
+
+echo Your heart's been aching, but
+ping localhost -n 5 > nul
+cls
+
+echo You're too shy to say it
+ping localhost -n 5 > nul
+cls
+
+echo Inside, we both know what's been going on
+ping localhost -n 5 > nul
+cls
+
+echo We know the game and we're gonna play it
+ping localhost -n 5 > nul
+cls
+
+echo And if you ask me how I'm feeling
+ping localhost -n 5 > nul
+cls
+
+echo Don't tell me you're too blind to see
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give you up
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna let you down
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna run around and desert you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna make you cry
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna say goodbye
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna tell a lie and hurt you
+ping localhost -n 5 > nul
+cls
+
+echo (Ooh, give you up)
+ping localhost -n 5 > nul
+cls
+
+echo (Ooh, give you up)
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give, never gonna give
+ping localhost -n 5 > nul
+cls
+
+echo (Give you up)
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give, never gonna give
+ping localhost -n 5 > nul
+cls
+
+echo (Give you up)
+ping localhost -n 5 > nul
+cls
+
+echo We've known each other for so long
+ping localhost -n 5 > nul
+cls
+
+echo Your heart's been aching, but
+ping localhost -n 5 > nul
+cls
+
+echo You're too shy to say it
+ping localhost -n 5 > nul
+cls
+
+echo Inside, we both know what's been going on
+ping localhost -n 5 > nul
+cls
+
+echo We know the game and we're gonna play it
+ping localhost -n 5 > nul
+cls
+
+echo I just wanna tell you how I'm feeling
+ping localhost -n 5 > nul
+cls
+
+echo Gotta make you understand
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give you up
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna let you down
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna run around and desert you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna make you cry
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna say goodbye
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna tell a lie and hurt you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give you up
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna let you down
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna run around and desert you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna make you cry
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna say goodbye
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna tell a lie and hurt you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna give you up
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna let you down
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna run around and desert you
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna make you cry
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna say goodbye
+ping localhost -n 5 > nul
+cls
+
+echo Never gonna tell a lie and hurt you
+ping localhost -n 5 > nul
+cls
+echo Ok, im done... What were those very fast moveing texts? Take a screenshot. :)
 pause
